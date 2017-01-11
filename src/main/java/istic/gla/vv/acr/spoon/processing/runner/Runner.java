14c41cc7228009cf75d0c51578619285fddd3b89
bs <bizimungu.steve@gmail>
2017-01-11 02:08:45 +0100

    final version correction

--- src/main/java/istic/gla/vv/acr/spoon/processing/runner/Runner.java
+++ src/main/java/istic/gla/vv/acr/spoon/processing/runner/Runner.java
@@ -8,17 +8,9 @@ import spoon.Launcher;
 public class Runner {
 	
 	public static void main(String[] args){
-//		String path = args[0];
-//		String outsrcPath= path+"/spooned";
+		String path = args[0];
+		String outsrcPath= path+"/spooned";
 //		
-		String path ="/home/steve/mworkspace/istic/vv/tp/testResources/wordbrain-solver/src/main";
-		String outsrcPath="/home/steve/mworkspace/istic/vv/workspace/wordbrainspooned/src/main";
-//		
-//		String path2 = "/home/steve/mworkspace/istic/vv/workspace/test/src/main";
-//		String path3 ="/home/steve/mworkspace/istic/vv/tp/testResources/pairwise/src/main";
-//		String outsrcPath3="/home/steve/mworkspace/istic/vv/tp/spoonedressources/pairwisespooned/src/main";
-//		String outsrcPath2="/home/steve/mworkspace/istic/vv/workspace/testspooned/";
-//		String outbinPath2="/home/steve/mworkspace/istic/vv/workspace/testspooned/";
 		Launcher launcher = new Launcher();
 		launcher.addInputResource(path);
 		launcher.setSourceOutputDirectory(outsrcPath);
